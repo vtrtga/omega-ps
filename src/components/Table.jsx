c3843624749255a15e1de2147592c93fa22953be
vtrtga <vitor.ypvalim@gmail.com>
2022-12-22 10:35:13 -0400

    erro de sincronicidade

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -2,34 +2,60 @@ import React, { useEffect, useState } from 'react';
 import ibgeApi from '../services/api';
 
 function Table() {
-  const [data, setData] = useState();
+  const [data, setData] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
-  const [dataFilter, setDataFilter] = useState([]);
+  // const [dataFilter, setDataFilter] = useState([]);
 
+  // eslint-disable-next-line consistent-return
   const dataRequest = async () => {
-    const response = await ibgeApi.get();
-    setData(response);
-    setIsLoading(false);
-    console.log(data);
-    return response;
+    try {
+      const response = await ibgeApi.get();
+      if (response.length > 0) {
+        setData(response.data);
+      }
+      setIsLoading(false);
+      console.log(data);
+      return response;
+    } catch (e) {
+      console.log(e);
+    }
   };
 
   useEffect(() => {
     dataRequest();
-  }, []);
+  }, [setData]);
   return (
-    <table>
+    // eslint-disable-next-line react/jsx-no-useless-fragment
+    <>
       {
-        isLoading ? (<p>Loading...</p>)
-          : (
-            <tr>
+      isLoading ? (<p>Loading...</p>)
+        : (
+          <table>
+            <thead>
+              <tr>
+                <th>Id</th>
+                <th>Nome</th>
+                <th>Região</th>
+                <th>Sigla</th>
+              </tr>
+            </thead>
+            <tbody>
+
               {
-          data[0].map((d) => <td>{ Object.keys(d) }</td>)
-        }
-            </tr>
-          )
+              data.map((item) => (
+                <tr key={item.id}>
+                  <td>{item.id}</td>
+                  <td>{item.nome}</td>
+                  <td>{item.regiao}</td>
+                  <td>{item.sigla}</td>
+                </tr>
+              ))
+          }
+            </tbody>
+          </table>
+        )
       }
-    </table>
+    </>
   );
 }
 
